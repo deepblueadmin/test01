8173f28a30145665415b168db0d715a9a66c1e38
zs <zs@bjpowernode.com>
2020-03-23 11:23:19 +0800

    test1模块初始提交

---
+++ GithubTest/src/com/bj/test/User.java
@@ -0,0 +1,15 @@
+package com.bj.test;
+
+/**
+ * Class Neme:User
+ * Package:com.bj.test
+ * Description:github测试
+ *
+ * @date:2020/03/23 11:03
+ * @author:靳强
+ */
+public class User {
+
+    private String id;
+
+}
